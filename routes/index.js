b1560c91213831dda73130c5ca535408e80443b9
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-14 00:55:13 -0300

    Refactoring code

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const { Router } = require('express');
+
+const router = Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var { Router } = require('express');
+
+var router = Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (_req, res) => {
   res.send('respond with a resource');
 });
 
