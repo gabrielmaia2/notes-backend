7f5faecbe7e7b47d5d89d0c4ec97429b8fe98a6a
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-14 01:07:23 -0300

    Update notes.js
    Create interface for getting notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,18 @@
 const { Router } = require('express');
+const db = require('../db');
 
 const router = Router();
 
-/* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+router.get('/get', (req, res) => {
+  const { userId, search } = req.body;
+
+  const searchPattern = db.escape(`%${search}%`);
+  let sql = 'SELECT id, title, content FROM note WHERE ';
+  sql += `title LIKE ${searchPattern} OR content LIKE ${searchPattern}`;
+
+  db.pool.query(sql, (err, notes) => {
+    res.json({ err, notes });
+  });
 });
 
 module.exports = router;
