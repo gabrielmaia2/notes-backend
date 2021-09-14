f938e8bde55d51e56fb9adf3f37ef894da7fad2d
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2021-09-14 01:05:40 -0300

    Create notes route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const tagsRouter = require('./routes/tags');
+const notesRouter = require('./routes/notes');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tags', tagsRouter);
+app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use((_req, _res, next) => {
